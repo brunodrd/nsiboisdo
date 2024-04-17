d1bc60730ecf63490427393698059cd070b1afcf
brunodrd <d-br-o@hotmail.fr>
2024-04-17 18:07:57 +0400

    update C10C4

---
+++ scripts/C10/rendudyn.py
@@ -0,0 +1,11 @@
+def rendudynamique(pieces, somme):
+    """
+    Renvoie le nombre de pieces minimal 'nb_piece' nécessaire pour former 'somme'
+    """
+    nb_piece = [0] * (somme+1) # initialisation du cache
+    for n in range(1, somme+1): # on effectue le calcul de tous les ss pb jusqu'à somme
+        nb_piece[n] = n
+        for p in pieces:
+            if p <= n:
+                nb_piece[n] = min(..., ...)
+    return nb_piece[somme]
---
+++ scripts/C10/rendudyn_test.py
@@ -0,0 +1,2 @@
+assert rendudynamique([1, 2, 5, 10, 20], 100) == 5
+assert rendudynamique([1, 2], 100) == 50
