3d348b04afcdd164c38b79f57e27cda6f46cc36b
brunodrd <d-br-o@hotmail.fr>
2024-07-02 08:23:28 +0400

    New C3 NSI1

---
+++ scripts/NSI1C3/division.py
@@ -0,0 +1,7 @@
+# Réaliser des divisions euclidiennes en python
+a = 17
+b = 2
+q = a // b
+r = a % b
+print("quotient: q = ", q, ", reste: r = ", r)
+print("concaténation de 'r' et '00':", str(r) + '00')
